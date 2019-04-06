d921f465ed97f96f6ba1d99c9f3093708397bd8d
kamilah sari fauziyah <49320786+kamilah4017@users.noreply.github.com>
2019-04-06 11:49:05 +0700

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,17 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+    Scanner sc = new Scanner (System.in);
+        System.out.print("Masukkan angka : ");
+        int angka = sc.nextInt();
 
-
-        for (int i=1;i<=4;i++){
-            for(int j=5;j>=i;j--){
+        for (int i=1;i<=angka;i++){
+            for(int j=angka+1;j>=i;j--){
                 System.out.print("0");
             }
             for(int k=1;k<=i;k++){
@@ -16,19 +20,19 @@ public class Main {
             for (int l=1;l<=i-1;l++){
                 System.out.print("*");
             }
-            for(int x=5;x>=i;x--){
+            for(int x=angka+1;x>=i;x--){
                 System.out.print("0");
             }
             System.out.println();
         }
-        for (int i=1;i<=5;i++){
+        for (int i=1;i<=angka+1;i++){
             for(int j=1;j<=i;j++){
                 System.out.print("0");
             }
-            for(int z=5;z>=i;z--){
+            for(int z=angka+1;z>=i;z--){
                 System.out.print("*");
             }
-            for(int k=4;k>=i;k--){
+            for(int k=angka;k>=i;k--){
                 System.out.print("*");
             }
             for(int x=1;x<=i;x++){
